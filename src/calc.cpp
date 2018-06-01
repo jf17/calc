06022f96036c4ca8b654b1dfe17709ff98753b00
Aleksey <javafirst17@gmail.com>
2018-06-02 00:06:38 +0300

    add new func

--- src/calc.cpp
+++ src/calc.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
-int main(){
+
+void plus_num(){
+
+    int a =0;
+    int b =0;
+    
+    cout<<"Enter an a : ";
+    cin>>a;
+    
+    cout<<"Enter an b : ";
+    cin>>b;
+    
+    cout<<"a + b = " << a + b << endl;
+
+
+}
+void multiplication_num(){
     
     int a =0;
     int b =0;
@@ -13,7 +29,21 @@ int main(){
     cout<<"Enter an b : ";
     cin>>b;
     
-    cout<<"a + b =" << a + b << endl;
+    cout<<"a + b = " << a * b << endl;
+    
+    
+}
+
+
+int main(){
+    
+    char a ='d';
+    
+    cout<<"What a function + or * ?"<< endl;
+    cin>>a;
+
+    if(a=='+'){ plus_num();}
+    else if (a=='*'){multiplication_num();}
     
     return 0;
 }
