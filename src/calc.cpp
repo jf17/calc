e6cde0c02047e38858693dea881a663f2b4e89e6
Aleksey <javafirst17@gmail.com>
2018-06-01 23:38:48 +0300

    create

---
+++ src/calc.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a =0;
+    int b =0;
+    
+    cout<<"Enter an a : ";
+    cin>>a;
+    
+    cout<<"Enter an b : ";
+    cin>>b;
+    
+    cout<<"a + b =" << a + b << endl;
+    
+    return 0;
+}
